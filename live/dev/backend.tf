95a062bce577cacc1575a3efa84e0a4b4a7b05d0
paolo.murino <paolo.murino@nttdata.com>
2025-11-05 09:56:35 +0100

    corretti bucket dove si trovava lo state

--- live/dev/backend.tf
+++ live/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terragrunt-to-terralith-tfstate-2025-09-24-2359"
+    bucket       = "terragrunt-to-terralith-tfstate-2025-11-03-2001"
     key          = "dev/tofu.tfstate"
     region       = "eu-south-1"
     encrypt      = true
